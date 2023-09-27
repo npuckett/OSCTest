8d06197e70615a6898f306ad2663cd3ada32b630
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2023-09-27 12:57:29 -0400

    Added LED code
    commented out due to strange disconnect issue

--- ArduinoUnityTest/send2RandVals_readXYZ_OSC_2/send2RandVals_readXYZ_OSC_2.ino
+++ ArduinoUnityTest/send2RandVals_readXYZ_OSC_2/send2RandVals_readXYZ_OSC_2.ino
@@ -3,6 +3,7 @@
 
 
 // WiFi stuff change it to whatever matches your network
+//const int LEDpin = 21;
 const char* ssid = "Khan_2.4G";
 const char* pwd = "9053387336";
 
@@ -19,18 +20,49 @@ const char* unityMessageTag = "/unity2";
 //port specified int eh OSC out variable in Unity
 const int myReceivePort = 9000;
 
-//this is the variables that are sent to unity
-float randomSend1;
-float randomSend2;
+IPAddress ip;
+String cnvIP;
 
-//values from Unity, the XYZ values of the sphere
-float movement;
-float movement2;
+//values from Unity, 
+float xDirection;
+float yDirection;
+
+
+///function called when data is received from Unity
+void unityOSCdata(const OscMessage& unityMessage) 
+{
+
+//read the 2 float values from Unity
+xDirection = unityMessage.arg<float>(0);
+yDirection = unityMessage.arg<float>(1);
+
+  Serial.print("::: RECEIVE ::: Unity ");
+  Serial.print(sendIPaddress);
+  Serial.print("-> Arduino IP: ");
+  Serial.print(cnvIP);
+  Serial.print(" Port: ");
+  Serial.print(myReceivePort);
+  Serial.print(" : ");
+  Serial.print(unityMessageTag);
+  Serial.print("\t");
+  Serial.print(xDirection);
+  Serial.print("\t");
+  Serial.print(yDirection);
+  Serial.println();
+
+  //pinMode(LEDpin, OUTPUT);
+  //int brightness = map(xDirection,-1,1,0,255);
+  //analogWrite(LEDpin,brightness);
+ 
+}
+
+
 
 
 
 void setup() 
 {
+    
     Serial.begin(9600);
 
 //connect to WiFi
@@ -42,84 +74,36 @@ void setup()
     Serial.print(" .connecting.");
     delay(500);
     }
+
+    ip = WiFi.localIP();
+    cnvIP = "IP: "+IpAddress2String(ip);
+
     Serial.print("WiFi connected, IP = ");
-    Serial.println(WiFi.localIP());
+    Serial.println(ip);
 
     //turns on the built in LED to show it conneted
     pinMode(LED_BUILTIN,OUTPUT);
     digitalWrite(LED_BUILTIN,HIGH);
 
-
+    
+ 
     //////******THE OUTPUTS*********//////
-    //sets up publishing the osc message with the 2 values from your defined variables 
-    //if you need to send more than 2 values you will need to add more variables and add them to the publish function
-    //you can mix different types of variables in the publish function
-    OscWiFi.publish(sendIPaddress, mySendPort, arduinoMessageTag, randomSend1, randomSend2);
-
-
-    //////******THE OUTPUTS*********//////
-    //the variables that receive the data should be in the same order as they were packaged
-    OscWiFi.subscribe(myReceivePort, unityMessageTag, movement, movement2);
-
+    //This binds the incoming data to a callback function (just like how it is set up on the unity side)
+    OscWiFi.subscribe(myReceivePort, unityMessageTag, unityOSCdata);
 
 }
 
 void loop() 
 {
-
-//calculates random numbers to send. replace this with sensor data
-randomSend1 = random(0,1000)/10.0;
-randomSend2 = random(0,1000)/10.0;
     
-    
-    OscWiFi.update();  // should be called to receive + send osc
+    //OscWiFi.update();  // should be called to receive + send osc
     // or do that separately
-    // OscWiFi.parse(); // to receive osc
+    OscWiFi.parse(); // to receive osc
     // OscWiFi.post(); // to publish osc
-
-    //print out the data to Serial
-    printOSCdata();
+   
 
 }
 
-
-
-void printOSCdata() 
-{
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  String cnvIP = "IP: "+IpAddress2String(ip);
-  Serial.print("::: SEND ::: Arduino ");
-  Serial.print(cnvIP);
-  Serial.print("-> Target IP: ");
-  Serial.print(sendIPaddress);
-  Serial.print(" Port: ");
-  Serial.print(mySendPort);
-  Serial.print(" : ");
-  Serial.print(arduinoMessageTag);
-  Serial.print("\t");
-  Serial.print(randomSend1);
-  Serial.print("\t");
-  Serial.println(randomSend1);
-
-  Serial.print("::: RECEIVE ::: Unity ");
-  Serial.print(sendIPaddress);
-  Serial.print("-> Arduino IP: ");
-  Serial.print(cnvIP);
-  Serial.print(" Port: ");
-  Serial.print(myReceivePort);
-  Serial.print(" : ");
-  Serial.print(unityMessageTag);
-  Serial.print("\t");
-  Serial.print(movement);
-  Serial.print("\t");
-  Serial.print(movement2);
-  //Serial.print("\t");
-  //Serial.println(tz);
-
-
-  
-}
 
 //helper function to convert the IP address to a string
 String IpAddress2String(const IPAddress& address)
